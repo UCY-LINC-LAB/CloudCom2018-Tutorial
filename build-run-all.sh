e11fe5561b7e50037dcb009ae5f3fbee2c35aafb
Thanasis Tryfonos <a.tryfonos@cs.ucy.ac.cy>
2018-09-25 15:40:48 +0300

    Dockerized ratings service and extended docker-compose

--- build-run-all.sh
+++ build-run-all.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 # Comment the following line in production -- Warning it clears all docker images and containers
-docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker rmi $(docker images -q)
+docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker rmi $(docker images -q) -f
+
+mvn clean install package
+
 
 docker-compose up -d
 
-sleep 10
 
-clear
 
-mvn clean install package
+#watch -n 1 'docker ps -a'
+
 
