d5ef541d4f333535013d9ccf4836ed4040480d09
Thanasis Tryfonos <a.tryfonos@cs.ucy.ac.cy>
2018-10-12 12:53:50 +0300

    Added traefik loadbalancer and connected it with consul

--- apigateway/src/main/java/cy/ac/ucy/linc/api/APIGateway.java
+++ apigateway/src/main/java/cy/ac/ucy/linc/api/APIGateway.java
@@ -3,10 +3,10 @@ package cy.ac.ucy.linc.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class APIGateway {
 
     public static void main(String[] args) {
--- build-run-all.sh
+++ build-run-all.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
 # Comment the following line in production -- Warning it clears all docker images and containers
-docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker rmi $(docker images -q) -f
-
+#docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker rmi $(docker images -q) -f
+#
 mvn clean install package
 
 
-docker-compose up -d
+docker-compose down --remove-orphans
 
+#docker-compose rm -f
 
+docker volume prune -f
+
+docker-compose up -d --force-recreate --remove-orphans --build
+
+docker-compose ps
 
 #watch -n 1 'docker ps -a'
 
