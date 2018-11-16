f8af9c130c9f10214b57c8fbfdf210d1b647e5c9
dtrihinas <trihinas@cs.ucy.ac.cy>
2018-11-16 12:14:09 +0200

    revert back to id

--- books/src/main/java/cy/ac/ucy/linc/microservices/books/repositories/BookRepository.java
+++ books/src/main/java/cy/ac/ucy/linc/microservices/books/repositories/BookRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface BookRepository extends CrudRepository<Book, Integer> {
-    Book findBookByTitle(String title);
+    Book findBookById(Long id);
 }
