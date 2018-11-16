e4ecb9b3b851fa03da22e5eddd54a4f850a7fff8
dtrihinas <trihinas@cs.ucy.ac.cy>
2018-11-16 12:13:26 +0200

    extended crud functionality

--- books/src/main/java/cy/ac/ucy/linc/microservices/books/repositories/BookRepository.java
+++ books/src/main/java/cy/ac/ucy/linc/microservices/books/repositories/BookRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface BookRepository extends CrudRepository<Book, Integer> {
-    Book findBookById(Long id);
+    Book findBookByTitle(String title);
 }
